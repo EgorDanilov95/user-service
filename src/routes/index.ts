595927429401ade1441c2588c363e7d1c7ab1763
Egor Danilov <danilov-egor-e@yandex.ru>
2026-05-21 04:02:25 +0300

    added routes to routes/index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,26 @@
 import Router from 'koa-router';
+import { authMiddleware } from '../middleware/auth';
+import { requireAdmin } from '../middleware/requireAdmin';
+import {
+  registerController,
+  loginController,
+  getUserByIdController,
+  getUsersListController,
+  blockUserController,
+} from '../controllers/userController';
 
 const router = new Router();
+
+
+router.post('/api/register', registerController);
+router.post('/api/login', loginController);
+
+
+router.get('/api/users/:id', authMiddleware, getUserByIdController);
+router.patch('/api/users/:id/block', authMiddleware, blockUserController);
+
+
+router.get('/api/users', authMiddleware, requireAdmin, getUsersListController);
 
 router.get('/health', (ctx) => {
   ctx.body = { status: 'ok' };
